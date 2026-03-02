8d6ebd0e7a6b3bb0ef5aae11026273e84d84384d
风雪心 <1097071510@qq.com>
2026-03-02 16:48:31 +0800

    feat: 增加了更新、复制和彻底删除功能的后端接口

--- mock/question.js
+++ mock/question.js
@@ -49,5 +49,38 @@ module.exports = [
                 }
             }
         }
+    },
+    {
+        //更新问卷
+        url:'/api/question/:id',
+        method:'patch',
+        response() {
+            return {
+                errno: 0
+            }
+        }
+    },
+    {
+        //复制问卷
+        url:'/api/question/duplicate/:id',
+        method:'post',
+        response(){
+            return {
+                errno:0,
+                data:{
+                    id:Random.id()
+                }
+            }
+        }
+    },
+    {
+        //批量彻底删除
+        url:'/api/question',
+        method:'delete',
+        response(){
+            return {
+                errno:0
+            }
+        }
     }
 ]
